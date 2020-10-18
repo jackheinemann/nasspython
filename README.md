982109d7a0c1b6e0dbdca8c3cb95461fff526cec
Jack Heinemann <jack@oenoke.com>
2020-10-18 16:26:19 -0400

    testing markdown for README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nass_python
-A wrapper for the NASS Quickstats API for python
+A wrapper for the NASS Quickstats API for python, modeled off of the R package documented here: https://github.com/rdinter/usdarnass
+
+## Functions provided:
---
+++ setup.py
@@ -0,0 +1,24 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="nass_python",
+    version="1.0.0",
+    description="Wrapper for the NASS Quickstats database",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jackheinemann/nass_python",
+    author="Jack Heinemann",
+    author_email="jack@oenoke.com"",
+    license="MIT",
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["requests"],
+)
