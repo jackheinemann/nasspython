a93e9856cbbfcf61911fea6f578d1874d34c076d
Jack Heinemann <jack@oenoke.com>
2020-10-18 16:07:56 -0400

    added basic __main__.py

---
+++ nass_python/__main__.py
@@ -0,0 +1,9 @@
+
+def main():
+    # no real main function makes sense for this package, so this will just print the github url
+
+    print('For usage instructions, go here: https://github.com/jackheinemann/nass_python')
+
+
+if __name__ == "__main__":
+    main()
